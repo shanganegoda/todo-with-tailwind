77651636e78bde893dc363997f8702675987797c
Shan <shanganegoda96@gmail.com>
2024-02-29 18:10:05 +0800

    Added all functionality check/delete and persistence and broke into components

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,36 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ToDoItem from './ToDoItem'
-import Checkbox from '@mui/material/Checkbox';
 
-function ToDoList({todoList}) {
 
-    // useEffect(() => {
-    //      todoList = localStorage.getItem(todoList);
-    // },[todoList])
-    const handleClick = () => {
-        setChecked(!checked);
-    }
-
+function ToDoList({todoList ,toggleTodo , deleteTodo}) {
 
     return (
+        
         <div id='to-do-list' className='flex flex-col gap-3 mb-5'>
+            {todoList.length === 0 && <span className='text-white font-semibold'>No To Dos</span>}
             {todoList?.map((todo, i) =>
-                ( <div className='flex bg-violet-600 p-4 mx-5 rounded-lg justify-between items-center'>
-                <div className='flex items-center'>
-                    <Checkbox onClick={handleClick} checked={checked} />
-                    <p className='text-white font-semibold ml-3'>{name}</p>
-                </div>
-                <div>
-                    <IconButton>
-                        <ModeEdit className='text-white' />
-                    </IconButton>
-                    <IconButton>
-                        <Delete className='text-white' />
-                    </IconButton>
-                </div>
-            </div>)
+            (<ToDoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />)
             )}
-        </div>
+          </div>
     )
 }
 
